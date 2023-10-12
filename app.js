5068cf0aa7051966453bd156eecbc67ebdd0c200
Adit Hernowo <adith@Adits-MacBook-Air.local>
2023-10-12 15:06:40 +0700

    rmv

--- app.js
+++ app.js
@@ -6,25 +6,20 @@ const fs = require('fs');
 const app = express();
 app.use(bodyParser.json());
 
-// Secret Key for JWT (should be stored securely)
 const secretKey = 'your-secret-key';
 
-// Read user data from users.json file
 const usersData = fs.readFileSync('user.json', 'utf-8');
 const users = JSON.parse(usersData);
 
-// Read user data from users.json file
 const teacherData = fs.readFileSync('teacher.json', 'utf-8');
 const teacher = JSON.parse(teacherData);
 
 
-// Login Route
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
     const user = users.find(u => u.username === username && u.password === password);
 
     if (user) {
-        // Create JWT token
         const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
         res.json({ token });
     } else {
@@ -32,7 +27,6 @@ app.post('/login', (req, res) => {
     }
 });
 
-// Authenticated Route
 app.get('/get-teachers', (req, res) => {
     const token = req.header('Authorization');
 
@@ -40,13 +34,11 @@ app.get('/get-teachers', (req, res) => {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
-    // Verify token
     jwt.verify(token, secretKey, (err, decoded) => {
         if (err) {
             return res.status(401).json({ message: 'Invalid token.' });
         }
 
-        // Token valid, proceed with operations requiring authentication here
         res.json({ message: 'Berhasil mengambil data', teacher: teacher });
     });
 });
